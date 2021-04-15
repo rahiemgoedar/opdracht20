84bdbd0d1eee21ad7c18c9e6333daf8f9941ff9c
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-15 18:26:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//Deel 1
+const age = 18;
+
+if (age > 17) {
+    console.log("Come inside, Please!");
+
+} else if (age < 18) {
+    console.log("Sorry, you are too young to come inside!");
+}
+
+//Deel 2
+const isFemale = true;
+
+if (true) {
+    console.log("Free ticket, because it's Ladies Night!");
+} else {
+    console.log("Hello Sir, you can buy a ticket over there!");
+}
+//Deel 3
+const driverStatus = "bob";
+
+if ("bob") {
+    console.log("Have a good night alcohol free, BOB!");
+} else {
+    console.log("Enjoy your drinks, but do not drive, when going home!");
+}
